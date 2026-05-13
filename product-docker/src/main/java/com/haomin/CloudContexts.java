0182275f9d308f800458a2440a425706a787a100
Haomin <haomiy@uw.edu>
2026-05-13 08:19:12 -0700

    Add files via upload

--- product-docker/src/main/java/com/haomin/CloudContexts.java
+++ product-docker/src/main/java/com/haomin/CloudContexts.java
@@ -10,6 +10,7 @@ public class CloudContexts {
         server.createContext("/files",    new FilesHandler(authManager, fileStorage));
         server.createContext("/upload",   new UploadHandler(authManager, fileStorage));
         server.createContext("/download", new DownloadHandler(authManager, fileStorage));
+        server.createContext("/oe",       new OeHandler(authManager, fileStorage));
         server.createContext("/",         new StaticFileHandler());
     }
 }
